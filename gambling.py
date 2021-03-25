ac983ee1d1d3146008f39897fe006ded35d773c0
matthewkellettonslow <79485867+matthewkellettonslow@users.noreply.github.com>
2021-03-25 17:51:43 +1300

    gambling, but modular :)
    not tested thoroughly for bugs, and not gone thru pep8

--- gambling.py
+++ gambling.py
@@ -1,127 +1,202 @@
 ##
-# gambling.py
-# Author: Matthew Kellett, Jacob Lum
-# Created: 3/03/2021
-# Version: 0.41
-from random import *
-choice = "y"
-SUITS = ['♤', '♡', '♧', '♢']
+# gambling2.py
+# Author: Matthew Kellett
+# Created: 17/03/2021
+# Version: 2.0
+import random
 history = ['♤', '♡', '♧', '♢']
-bet = 0
-BET_LIMITS = [0, 99999999]
-REDBLACK_GUESS = ["black", "red"]
-SUITS_GUESS = ["spades", "hearts", "clubs", "diamonds"]
 
 
-# Bet function by Jacob
-def money_input():
-    bet = -1
-    while bet <= BET_LIMITS[0] or bet >= BET_LIMITS[1]:
-        while True:
-            try:
-                bet = float(input("How much would you like to bet?: "))
+# Game input function
+def game_input():
+    """
+    Gets game choice from user
+    """
+    game = None
+    GAME_CHOICES = ["colours", "suits"]
+    # Gets game from user
+    while game not in GAME_CHOICES:
+        game = input("What game would you like to play? " +
+                     "(suits, colours): ").lower()
+        if game not in GAME_CHOICES:
+            print("ERROR: Please enter one of the listed games")
+    return game
+
+
+# Bet input function
+def bet_input():
+    """
+    Takes bet from user
+    """
+    BET_LIMITS = [0, 99999999]
+    repeat = True
+    # Takes bet from user
+    while repeat == True:
+        try:
+            bet = float(input("How much would you like to bet?: "))
+            if bet > BET_LIMITS[0] and bet < BET_LIMITS[1]:
                 break
-            except:
-                print("Please enter a number")
-        if bet <= BET_LIMITS[0] or bet >= BET_LIMITS[1]:
-            print("Please enter a reasonable bet")
+            else:
+                raise ValueError
+        except:
+            print("Please enter a number above 0 and below 99,999,999.")
     return bet
 
-while choice != "n":
 
-    # Error checking by Matthew
-    while True:
-        game = input("What game would you to play (redblack, suits): ")
-        if game != "redblack" and game != "suits":
-            print("ERROR: Please enter redblack or suits.")
-        else:
-            break
-
-# Fancy box by Jacob
+# History function
+def draw_history():
+    """
+    Prints out history of up to 4 from list
+    """
+    # add 4 rand values to history if empty
+    if len(history) == 0:
+        for i in range(0, 3):
+            history.append(random.choice(SUITS))
+    # print history
     print("History:")
-    print("┏", "━"*len(history), "┓")
+    print("┏", "━"* 4, "┓")
     print("┃", " ".join(history), "┃")
-    print("┗", "━"*len(history), "┛")
+    print("┗", "━"* 4, "┛")
 
-    # Redblack by Matthew and Jacob
-    if game == "redblack":
-        if bet == 0:
-            bet = money_input()
+
+def draw_card(SUITS):
+    """
+    Draws a card for gambling
+    """
+    # draw card
+    card = random.choice(SUITS)
+    return card
+
+# Colours game guess
+def colours_guess():
+    """
+    Gets a guess as an input from user
+    for colours game
+    """
+    COLOURS_GUESSES = ["black", "red"]
+    # Takes guess from user and checks for error
+    guess = None
+    while guess not in COLOURS_GUESSES:
+        guess = input("What is your guess? (black, red): ").lower()
+        if guess not in COLOURS_GUESSES:
+            print("ERROR: Please enter a valid guess.")
         else:
-            print("Your bet is ${}".format(bet))
-        # Redblack error checking by Jacob
-        guess = "no"
-        while guess not in REDBLACK_GUESS:
-            guess = input("What do you guess? ").lower()
-            if guess not in REDBLACK_GUESS:
-                print("Please enter a valid guess")
-        card = SUITS[randint(0, 3)]
-        history.append(card)
-        history.pop(0)
+            return guess
 
-        # Redblack by Matthew
-        print(card)
-        if (SUITS.index(card) == 0 or SUITS.index(card) == 2 and
-                guess == "black"):
-            print("You won!")
-            bet *= 2
-        elif (SUITS.index(card) == 1 or SUITS.index(card) == 3 and
-                guess == "red"):
-            print("You won!")
-            bet *= 2
+
+# Colours game win/lose
+def colours_check(card, bet, guess, SUITS):
+    """
+    Checks if card drawn matches guess
+    then returns bet for colours game
+    """
+    # Print "card" drawn from deck
+    print("Card drawn: {}".format(card))
+    SUITS.index(card) % 2
+    # Check if guess is correct and output result
+    if guess == "black" and SUITS.index(card) % 2 == 0:
+        print("You won!")
+        return bet * 2
+    elif guess == "red" and SUITS.index(card) % 2 == 1:
+        print("You won!")
+        return bet * 2
+    else:
+        print("You lose...")
+        return 0
+
+
+# Suits game guess
+def suits_guess(SUITS_GUESSES):
+    """
+    Gets a guess as an input from user
+    for suits game
+    """
+    # Takes guess from user and checks for error
+    guess = None
+    while guess not in SUITS_GUESSES:
+        guess = input("What is your guess? " +
+                      "(spades, hearts, clubs, diamonds): ").lower()
+        if guess not in SUITS_GUESSES:
+            print("ERROR: Please enter a valid guess.")
         else:
-            print("You lose!")
-            bet = 0
+            return guess
 
-    # Suits by Jacob and Matthew
-    elif game == "suits":
-        if bet == 0:
-            bet = money_input()
-        else:
-            print("Your bet is ${}".format(bet))
-        # Suits error checking by Matthew
 
-        while True:
-            guess = input("What do you guess? ")
-            if guess not in SUITS_GUESS:
-                print("ERROR: Please enter one of the suits\
- (diamonds, hearts, spades, clubs)")
-            else:
-                break
-        card = SUITS[randint(0, 3)]
-        history.append(card)
-        history.pop(0)
-        print(card)
+# Suits game win/lose
+def suits_check(card, bet, guess, SUITS, SUITS_GUESSES):
+    """
+    Checks if card drawn matches guess
+    then returns bet for suits game
+    """
+    # Print card drawn
+    print("Card drawn: {}".format(card))
+    # Check if guess is correct and output result
+    if SUITS.index(card) == SUITS_GUESSES.index(guess):
+        print("You win!")
+        return bet * 4
+    else:
+        print("You lose...")
+        return 0
 
-        # Suits guess check by Jacob
-        if SUITS.index(card) == SUITS_GUESS.index(guess):
-                print("You Win")
-                bet *= 4
-        else:
-            bet = 0
-            print("You lose")
 
-    # Check if user won or not
-    if bet > 0:
-        while True:
-            choice = input("Your balance is {}. Would\
- you like to gamble? (y/n) ".format(bet))
-            if choice == "n":
+# Checks if user wants to gamble/play again
+def user_won(bet):
+    """
+    Checks if user wants to play again
+    """
+    gamble_choice = None
+    repeat_choice = None
+
+    if bet != 0:
+        # Check if user wants to gamble
+        while gamble_choice != "y" and gamble_choice != "n":
+            gamble_choice = input("Your balance is ${}. Would ".format(bet) +
+                           "you like to gamble? (y/n): ").lower()
+            if gamble_choice == "y":
+                return gamble_choice
+            elif gamble_choice == "n":
                 print("You got ${}!".format(bet))
                 break
-            elif choice == "y":
-                break
             else:
-                print("ERROR: Please enter a proper answer (y/n)")
-    else:
-        print("GAME OVER")
-        # Check if user wants to play again
-        while True:
-            choice = input("Do you want to play again? (y/n) ")
-            if choice == "y":
-                break
-            elif choice == "n":
-                print("Thanks for playing.")
-                break
-            else:
-                print("ERROR: Please enter a proper answer (y/n) ")
+                print("ERROR: Please enter a proper answer")
+
+    # Check if user wants to play again
+    while repeat_choice != "y" and repeat_choice != "n":
+        repeat_choice = input("Do you want to play again? (y/n): ").lower()
+        if repeat_choice == "y" or repeat_choice == "n":
+            print("Thanks for playing!")
+            return repeat_choice
+        else:
+            print("ERROR: Please enter a proper answer")
+
+
+# Gamble menu function
+def gamble_menu():
+    SUITS = ['♤', '♡', '♧', '♢']
+    history = []
+    SUITS_GUESSES = ["spades", "hearts", "clubs", "diamonds"]
+    repeat_choice = None
+    bet = 0
+
+    # Get game, bet from user and print history
+    while repeat_choice != "n":
+        game = game_input()
+        draw_history()
+        card = draw_card(SUITS)
+        if bet == 0:
+            bet = bet_input()
+        # If game is colours, play colours
+        if game == "colours":
+            guess = colours_guess()
+            bet = colours_check(card, bet, guess, SUITS)
+        # If game is suits, play suits.
+        elif game == "suits":
+            guess = suits_guess(SUITS_GUESSES)
+            bet = suits_check(card, bet, guess, SUITS, SUITS_GUESSES)
+        # Check if user won and ask if they want to play again
+        repeat_choice = user_won(bet)
+
+if __name__ == "__main__":
+    gamble_menu()
+
+
